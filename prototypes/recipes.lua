b590b77d9fe5bdbc7f708a85da2a773e8012fb86
Ashayazu <remcohaak@outlook.com>
2024-10-30 22:44:16 +0100

    1.0.31

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -23,11 +23,8 @@ data:extend(
 			{type = "item", name ="iron-stick", amount = 2},
 			{type = "item", name ="iron-plate", amount = 2}
 		},
-		results = { {
-                type = "item",
-                name = "short-balloon-light",
-                amount = 1,
-            } },
+		results = { 
+			{type = "item", name = "short-balloon-light", amount = 1,} },
 	},
 	----------------------------------------------------------------------------------
 	{
@@ -36,15 +33,11 @@ data:extend(
 		enabled = false,
 		energy_required = 10,
 		ingredients = {
-			{type = "item", name ="electronic-circuit", amount = 2},
-			{type = "item", name ="iron-stick", amount = 2},
-			{type = "item", name ="iron-plate", amount = 2}
+			{type = "item", name ="advanced-circuit", amount = 10},
+			{type = "item", name ="steel-plate", amount = 20}
 		},
-		results = { {
-                type = "item",
-                name = "perfect-night-glasses",
-                amount = 1,
-            } },
+		results = {
+               {type = "item", name = "perfect-night-glasses", amount = 1,} },
 	},
 	
 }
